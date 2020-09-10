977a8af2496568dceedc2f81af75964e5ea35f06
Danilo <danillo.ifpb@gmail.com>
2020-09-10 12:48:02 -0300

    Update script.js

--- cronometro/script.js
+++ cronometro/script.js
@@ -1,21 +1,34 @@
-var btnReiniciar = document.getElementById("btn-reiniciar");
 var btnIniciar = document.getElementById("btn-iniciar");
 var btnPausar = document.getElementById("btn-pausar");
+var btnContinuar = document.getElementById("btn-continuar");
+var btnReiniciar = document.getElementById("btn-reiniciar");
 var counterView = document.getElementById("counterView");
 
-btnIniciar.addEventListener("click", iniciar);
+btnIniciar.addEventListener("click", () => {
+    iniciar(0, 0, 0);
+});
+
 btnPausar.addEventListener("click", pausar);
+btnPausar.style.display = "none";
 btnReiniciar.addEventListener("click", reiniciar);
+btnReiniciar.style.display = "none";
+btnContinuar.addEventListener("click", continuar);
+btnContinuar.style.display = "none";
+
 
 var textHoras = document.getElementById("horas");
 var textMinutos = document.getElementById("minutos");
 var textSegundos = document.getElementById("segundos");
 
-function iniciar() {
+function iniciar(h, m, s) {
+    btnIniciar.style.display = "none";
+    btnPausar.style.display = "block";
+    btnContinuar.style.display = "none";
+    btnReiniciar.style.display = "none";
 
-    segs = 0;
-    mins = 0;
-    hors = 0;
+    segs = s;
+    mins = m;
+    hors = h;
 
     contador = setInterval(() => {
         textHoras.innerText = hors < 10 ? "0" + hors : hors;;
@@ -37,11 +50,18 @@ function iniciar() {
 };
 
 function pausar() {
+    btnPausar.style.display = "none";
     clearInterval(contador);
+    btnContinuar.style.display = "block";
+    btnReiniciar.style.display = "block";
+};
+
+function continuar() {
+    iniciar(hors, mins, segs); 
 };
 
 function reiniciar() {
     clearInterval(contador);
-    iniciar(); 
+    iniciar(0, 0, 0); 
 };
 
